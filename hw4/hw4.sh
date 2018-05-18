a5a4122aa39abd9052c2b04ebfcca33c4067b18c
pixiyM <wangzx1994@gmail.com>
2018-05-18 22:32:02 +0800

    update hw4.sh

--- hw4/hw4.sh
+++ hw4/hw4.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo >test VAE
+echo test VAE
 python3 testVAE.py --data-path $1 --dir $2 --batch-size 1000
 
-echo >test GAN
+echo test GAN
 python3 testGAN.py --dir $2
 
-echo >test ACGAN
+echo test ACGAN
 python3 testACGAN.py --dir $2
